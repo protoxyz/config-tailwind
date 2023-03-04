b6ef1471b9b8607f8c8cbbb913f14056ece5725b
Eric Campbell <ericcampbell59@gmail.com>
2023-03-04 07:59:02 -0600

    change purge to content

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: [
+  content: [
     "./src/app/**/*.{js,ts,jsx,tsx}",
     "./src/pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
